870a31746cc62ef87f33c581d7d7ed8f974645ef
SY223 <akannisolomon810@gmail.com>
2026-01-17 19:54:15 +0000

    create post with image endpoint

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from routes import users, post
 
 app = FastAPI(title="Mini social media feed")
 
-app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(post.router, prefix="/posts", tags=["Posts"])
+app.include_router(users.user_router, prefix="/users", tags=["Users"])
+app.include_router(post.post_router, prefix="/posts", tags=["Posts"])
--- routes/users.py
+++ routes/users.py
@@ -1,24 +1,32 @@
-from fastapi import APIRouter, status
-from models import UserCreate
-router = APIRouter()
+from fastapi import APIRouter, status, HTTPException
+from schema.schema import UserCreate, UserOut
+from datetime import datetime, timezone
+from core.db import users_db
 
-users_db = {}
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+
+user_router = APIRouter()
+
+
+@user_router.post("/", status_code=status.HTTP_201_CREATED)
 def create_user(user: UserCreate):
     #extra  validation
     for id, details in users_db.items():
        if details.username == user.username:
-           raise ValueError("Username already exist!")
+           raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exist!")
     if not "@" in user.email or not ".com" in user.email:
         raise ValueError("Email not valid!")
     if not len(user.password) >= 8:
         raise ValueError("Password must be up to 8 characters")
     
-    user_id=len(users_db) + 1
-    db_user = user.model_dump()
-    db_user["id"] = user_id
-    users_db[user_id] =db_user
+    user_id=str(len(users_db) + 1)
+    new_user = UserOut(
+        id=user_id,
+        email=user.email,
+        username=user.username,
+        created_at=datetime.now(timezone.utc)
+    )
+    users_db[user_id] = new_user
 
     return {"message":"User successfully registered",
-            "details": {"username": db_user["username"], "email": db_user["email"]}}
+            "details": {"username": new_user.username, "email": new_user.email}}
