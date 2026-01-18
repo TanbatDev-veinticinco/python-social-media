ac1ef6b0f4cf4a7d042cbbe1376ea4d6f62ddba0
Alphaexcel <ojigbareoyinemi2@gmail.com>
2026-01-18 14:50:59 -0800

    getUser,UpdateUser and DeleteUser added

--- models.py
+++ models.py
@@ -11,3 +11,12 @@ class Post(BaseModel):
     content: str
     image: Optional[str] = None
     likes: int = 0
+
+# Update on models.py to include UserCreate and UserOut
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+
+class UserOut(BaseModel):
+    email: Optional[str] = None
