22db77c7ed76be6c4cc52f6bcaa21c2dca1b0a0a
SY223 <akannisolomon810@gmail.com>
2026-01-18 18:00:34 +0000

    router versioned

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from routes import users, post
 
 app = FastAPI(title="Mini social media feed")
 
-app.include_router(users.user_router, prefix="api/v1/users", tags=["Users"])
-app.include_router(post.post_router, prefix="api/v1/posts", tags=["Posts"])
+#Versioning of API: Stood at Version 1
+app.include_router(users.user_router, prefix="/api/v1/users", tags=["Users"])
+app.include_router(post.post_router, prefix="/api/v1/posts", tags=["Posts"])
