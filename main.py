e503e3607295ea66724c2b688f6421124864cb6c
Kevin kipkemoi <ronohkipkemoi79@gmail.com>
2026-01-22 21:00:05 +0300

    init

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from routes import users, post
 
 app = FastAPI(title="Mini social media feed")
 
+@app.get("/")
+def home():
+    return {"message": "Welcome to Mini Social Media Feed API"}
+
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(post.router, prefix="/posts", tags=["Posts"])
 
